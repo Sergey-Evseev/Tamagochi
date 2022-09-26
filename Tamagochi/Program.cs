171f917fff5c56fcb70b4c1af398429cc081f0c2
Sergey Evseev <sevseev@gmail.com>
2022-09-26 21:02:26 +0300

    Добавьте файлы проекта.

---
+++ Tamagochi/Program.cs
@@ -0,0 +1,25 @@
+﻿using Org.BouncyCastle.Crypto.Tls;
+using Org.BouncyCastle.Tls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
+
+namespace Tamagochi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DialogResult result = MessageBox.Show("Message", "Тамагочи", MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Information)
+            
+
+
+        }
+    }
+}
